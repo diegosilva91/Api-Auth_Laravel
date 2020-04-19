b126369b0cce960a2457cb7500bc37c08296d623
diegosilva91 <diegoarturosilva@hotmail.com>
2020-04-19 18:11:48 +0200

    post model and migrate with relationship

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title', 'body', 'user_id'
+    ];
+
+    public function users(){
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'is_admin' => 1
+            , 'name' => 'Manash'
+            , 'email' => 'manash.pstu@gmail.com'
+            , 'password' => bcrypt('secret')
+        ]);
+        factory(App\User::class, 50)->create()->each(function ($user){
+            for($i = 0; $i < 5; $i++){
+                $user->posts()->save(factory(App\Post::class)->create());
+            }            
+        });
     }
 }
