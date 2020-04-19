5a8e8de9970c81c70d73b6d82dc42037848d16ad
diegosilva91 <diegoarturosilva@hotmail.com>
2020-04-19 18:15:42 +0200

    post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        //
+        'title' => $faker->sentence
+        , 'body' => $faker->paragraph
+        , 'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
